6de0e11010cd0dde4620a69814346c03e98f4dd4
mrzhangboss <2529450174@qq.com>
2021-02-28 11:49:53 +0800

    :bug: 删除无用用户

--- master-slave/master_init.sql
+++ master-slave/master_init.sql
@@ -6,7 +6,3 @@ CREATE USER 'test'@'localhost' IDENTIFIED BY '123456';
 REVOKE all ON *.* FROM 'test'@'localhost';
 grant select,insert,update,delete on *.* to 'test'@'localhost' identified by '123456';
 
-
-CREATE USER 'test2'@'localhost' IDENTIFIED BY '123456';
-REVOKE all ON *.* FROM 'test2'@'localhost';
-grant select,insert,update,delete on *.* to 'test2'@'localhost' identified by '123456';
